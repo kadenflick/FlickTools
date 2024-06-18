853fab94418b0a7d2c1b0c775de44a4c31fd290c
Hayden Welch <hwelch@finelines-engineering.com>
2024-06-18 18:48:45 -0400

    move globals to class

--- tools/utility/VersionControl.py
+++ tools/utility/VersionControl.py
@@ -6,50 +6,28 @@ from utils.tool import Tool
 from utils import archelp
 from utils import models
 
-WORKDIR = os.path.join(os.path.dirname(__file__), "..")
-BRANCHES = \
-    subprocess.run(
-        ["git", "branch", "-a"], 
-        cwd=WORKDIR, 
-        capture_output=True, 
-        text=True
-    ).stdout.strip().replace('*',' ').split("\n  ")
-
-def get_status() -> str:
-    try:
-        status = \
-            subprocess.run(
-                ["git", "status"], 
-                cwd=WORKDIR, 
-                capture_output=True, 
-                text=True
-            ).stdout.strip()
-        return status
-    except subprocess.CalledProcessError as e:
-        return f"Error: {e}"
-
 class VersionControl(Tool):
-    
+    WORKDIR = os.path.join(os.path.dirname(__file__), "..")
+    BRANCHES = \
+        subprocess.run(
+            ["git", "branch", "-a"], 
+            cwd=WORKDIR, 
+            capture_output=True,
+            text=True
+        ).stdout.strip().replace('*',' ').split("\n  ")
+    ACTIVE_BRANCH = \
+        subprocess.run(
+            ["git", "branch", "--show-current"], 
+            cwd=WORKDIR, 
+            capture_output=True,
+            text=True
+        ).stdout.strip()
+        
     def __init__(self) -> None:
         super().__init__()
-        self.label = "Update Toolboxes"
-        self.description = "Runs a git pull on the toolboxes directory to update the tools."
+        self.label = f"Version Control ({VersionControl.ACTIVE_BRANCH})"
+        self.description = "Pulls the latest changes from the remote repository or switches to a different branch."
         self.category = "Verson Control"
-        
-        self.toolboxdir = WORKDIR
-        try:
-            self.active_branch = \
-                subprocess.run(
-                    ["git", "branch", "--show-current"], 
-                    cwd=self.toolboxdir, 
-                    capture_output=True,
-                    text=True
-                    ).stdout.strip()
-            self.branches = [branch.strip() for branch in BRANCHES]
-        except subprocess.CalledProcessError as e:
-            self.branches = ["Error"]
-            self.active_branch = "Error"
-            self.status = e
         return
     
     def getParameterInfo(self) -> list:
@@ -59,11 +37,11 @@ class VersionControl(Tool):
             datatype="GPString",
             parameterType="Optional",
             direction="Input",
-            category=self.active_branch,
+            category=VersionControl.ACTIVE_BRANCH,
         )
-        branch.value = self.active_branch
+        branch.value = VersionControl.ACTIVE_BRANCH
         branch.filter.type = "ValueList"
-        branch.filter.list = self.branches
+        branch.filter.list = VersionControl.BRANCHES
         
         status = arcpy.Parameter(
             displayName="Status",
@@ -71,39 +49,72 @@ class VersionControl(Tool):
             datatype="GPString",
             parameterType="Optional",
             direction="Input",
-            category=self.active_branch
+            category=VersionControl.ACTIVE_BRANCH,
         )
         status.controlCLSID='{E5456E51-0C41-4797-9EE4-5269820C6F0E}'
-        status.value = get_status()
+        status.value = self.get_status()
         
-        return [branch, status]
+        pull = arcpy.Parameter(
+            displayName="Pull",
+            name="pull",
+            datatype="GPBoolean",
+            parameterType="Optional",
+            direction="Input",
+            category=VersionControl.ACTIVE_BRANCH,
+        )
+        pull.value = True
+        
+        return [branch, pull, status]
     
     def updateParameters(self, parameters: list) -> None:
         params = archelp.Parameters(parameters)
         
-        if self.active_branch != params.branch.valueAsText:
-            # Checkout the selected branch
-            try:
-                subprocess.run(
-                    ["git", "checkout", params.branch.valueAsText],
-                    cwd=self.toolboxdir,
-                    capture_output=True,
-                    text=True
-                )
-                self.updateParameters(parameters)
-            except subprocess.CalledProcessError as e:
-                params.branch.addErrorMessage(f"Error checking out branch: {e}")
+        if params.branch.value != VersionControl.ACTIVE_BRANCH:
+            params.pull.value = False
+            params.pull.enabled = False
+        else:
+            params.pull.enabled = True
         
-        params.status.value = get_status()
-        
-        return parameters
+        return
     
     def execute(self, parameters:list, messages:list) -> None:
-        # Get the path to the toolboxes directory
-        toolbox_dir = os.path.join(os.path.dirname(__file__), "..")
-        # Run git pull
-        result = subprocess.run(["git", "pull"], cwd=toolbox_dir, capture_output=True, text=True)
-        archelp.message(result.stdout)
-        if result.stderr:
-            archelp.message(result.stderr, "error")
+        params = archelp.Parameters(parameters)
+        
+        if params.pull:
+            result =\
+                subprocess.run(
+                    ["git", "pull"], cwd=VersionControl.WORKDIR, 
+                    capture_output=True, 
+                    text=True
+                )
+            archelp.message(result.stdout)
+            if result.stderr:
+                archelp.message(result.stderr, "error")
+        elif VersionControl.ACTIVE_BRANCH != params.branch.value:
+            result =\
+                subprocess.run(
+                    ["git", "checkout", params.branch], 
+                    cwd=VersionControl.WORKDIR, 
+                    capture_output=True, 
+                    text=True
+                )
+            archelp.message(result.stdout)
+            if result.stderr:
+                archelp.message(result.stderr, "error")
+        
+        archelp.message(self.get_status())
+        
+    def get_status(self) -> str:
+        try:
+            status = \
+                subprocess.run(
+                    ["git", "status"], 
+                    cwd=VersionControl.WORKDIR, 
+                    capture_output=True,
+                    text=True
+                ).stdout.strip()
+            return status
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
+        
         return
